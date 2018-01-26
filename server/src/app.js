98dee1801e1cf95ecaca93cfa0503f883b6589b5
John Vaughn <johnjvaughn@gmail.com>
2018-01-26 13:29:59 -0500

    End of part 1, server/client both basic setup

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,12 @@ app.use(cors())
 
 app.get('/status', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: 'yo world'
   })
 })
-app.listen(process.env.PORT || 8082)
+app.post('/register', (req, res) => {
+  res.send({
+    message: 'Hello ' + req.body.email + '! Your user was registered. Have fun!'
+  })
+})
+app.listen(process.env.PORT || 8081)
